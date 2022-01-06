827f3067047bbb57d2a84c1a27481f242df6d814
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2022-01-06 23:56:05 +0100

    portfolio layout v1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,191 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <!-- <v-navigation-drawer app>
+      <v-row
+     
+      >
+        <v-col
+        class="profile-image"
+ 
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+          </v-avatar>
+        </v-col>
+      </v-row>
+ 
+  
+    
+  </v-navigation-drawer> -->
+
+    <v-main class="aside">
+      <v-row class="float-right-social">
+        <v-btn icon class="icon-spacing">
+          <v-icon large>mdi-linkedin</v-icon>
+        </v-btn>
+
+        <v-btn icon class="icon-spacing">
+          <v-icon large>mdi-facebook</v-icon>
+        </v-btn>
+
+        <v-btn icon class="icon-spacing">
+          <v-icon large>mdi-github</v-icon>
+        </v-btn>
+
+        <v-btn icon class="icon-spacing">
+          <v-icon large>mdi-email-send</v-icon>
+        </v-btn>
+      </v-row>
+
+      <v-row>
+        <v-col md="3" sm="4" xm="12">
+          <v-row class="profile-image">
+            <v-avatar class="profile-image" color="grey" size="164" tile>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
+              ></v-img>
+            </v-avatar>
+          </v-row>
+          <v-container fluid class="padding-filtre">
+            <!-- Languages -->
+            <v-row class="display-list">
+              <h4 class="filtre-subtitle">Languagues</h4>
+              <v-checkbox
+                class="checkbox-spacing"
+                v-model="checkbox1"
+                :label="`Checkbox 1: ${checkbox1.toString()}`"
+              ></v-checkbox>
+              <v-checkbox
+                class="checkbox-spacing"
+                v-model="checkbox2"
+                :label="`Checkbox 2: ${checkbox2.toString()}`"
+              ></v-checkbox>
+              <v-checkbox
+                class="checkbox-spacing"
+                v-model="checkbox2"
+                :label="`Checkbox 2: ${checkbox2.toString()}`"
+              ></v-checkbox>
+            </v-row>
+            <!-- Frameworks -->
+            <v-row class="display-list">
+              <h4 class="filtre-subtitle">Frameworks</h4>
+              <v-checkbox
+                class="checkbox-spacing"
+                v-model="checkbox1"
+                :label="`Checkbox 1: ${checkbox1.toString()}`"
+              ></v-checkbox>
+              <v-checkbox
+                class="checkbox-spacing"
+                v-model="checkbox2"
+                :label="`Checkbox 2: ${checkbox2.toString()}`"
+              ></v-checkbox>
+              <v-checkbox
+                class="checkbox-spacing"
+                v-model="checkbox2"
+                :label="`Checkbox 2: ${checkbox2.toString()}`"
+              ></v-checkbox>
+            </v-row>
+            <!-- Frameworks -->
+            <v-row class="display-list">
+              <h4 class="filtre-subtitle">Databases</h4>
+              <v-checkbox
+                class="checkbox-spacing"
+                v-model="checkbox1"
+                :label="`Checkbox 1: ${checkbox1.toString()}`"
+              ></v-checkbox>
+              <v-checkbox
+                class="checkbox-spacing"
+                v-model="checkbox2"
+                :label="`Checkbox 2: ${checkbox2.toString()}`"
+              ></v-checkbox>
+              <v-checkbox
+                class="checkbox-spacing"
+                v-model="checkbox2"
+                :label="`Checkbox 2: ${checkbox2.toString()}`"
+              ></v-checkbox>
+            </v-row>
+          </v-container>
+        </v-col>
+        <v-col md="9" sm="8" xs="12">
+          <HelloWorld />
+
+          <TabsList />
+        </v-col>
+      </v-row>
+    </v-main>
+    <v-footer app>
+      <!-- -->
+      FOOTER
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabsList from "./components/TabsList.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TabsList,
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      checkbox1: true,
+      checkbox2: false,
+    };
+  },
+};
+</script>
+<style scoped>
+.profile-image {
+  justify-content: start;
+  display: flex;
+
+  margin-bottom: 10%;
+}
+.padding-filtre {
+  padding: 0 0 0 0;
+}
+@media only screen and (max-device-width: 480px) {
+  .profile-image {
+    justify-content: center;
+    display: flex;
+    margin: 5% 0;
+  }
+  .padding-filtre {
+    margin: 0 5%;
   }
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.checkbox-spacing {
+  margin-top: 0px;
+  padding-top: 0px;
+}
+.filtre-subtitle {
+  margin-bottom: 1rem;
+}
+.display-list {
+  display: block;
+}
+.float-right-social {
+  display: flex;
+  justify-content: end;
+  align-content: flex-end;
+  width: 100%;
+  height: 3.5rem;
+  align-items: center;
+}
+.icon-spacing {
+  margin-right: 5px;
+}
+.aside {
+  margin: 0 5% 0 5%;
 }
 </style>
---
+++ src/components/TabsList.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card>
+    <v-tabs color="deep-purple accent-4" right>
+      <v-tab>Landscape</v-tab>
+      <v-tab>City</v-tab>
+      <v-tab>Abstract</v-tab>
+
+      <v-tab-item v-for="n in 3" :key="n">
+        <v-container fluid>
+          <v-row dense>
+            <v-col v-for="i in 3" :key="i" cols="12">
+              <v-card color="#385F73" dark shaped>
+                <v-card-title class="text-h5">
+                  Unlimited music now
+                </v-card-title>
+
+                <v-card-subtitle
+                  >Listen to your favorite artists and albums whenever and
+                  wherever, online and offline.</v-card-subtitle
+                >
+
+                <v-card-actions>
+                  <v-btn text> Listen Now </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "Tabslist",
+};
+</script>
